19114d91c768fc33c09516e941f2d5f68e2f9173
“LeeSuMin” <tn9330@gmail.com>
2024-01-29 21:38:53 +0900

    UPDATED:new files

---
+++ ch02.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+
+import streamlit as st
+#from ch01 import main
+import seaborn as sns
+import pandas as pd
+
+@st.cache_data #decorator, 함수의 중급 레벨 사용법
+def load_data():
+    #df = sns.load_dataset("iris")
+    df = sns.load_dataset("tips")
+    return df
+
+def main():
+    st.write(st.__version__)
+    st.write(sns.__version__)
+    st.write(pd.__version__)
+
+    tips = load_data()
+    #iris = load_data()
+    #st.dataframe(iris, use_container_width = True)
+    st.dataframe(tips, use_container_width = True)
+    st.write("-"*50)
+    st.title("st.metric()")
+    tip_max = tips['tip'].max()
+    tip_min = tips['tip'].min()
+
+    st.metric(label = "팁 최고 금액", value = tip_max, delta =tip_max-tip_min)
+    st.metric(label = "팁 최소 금액", value = tip_min, delta =tip_min-tip_max)
+
+if __name__ =="__main__":
+    main()
